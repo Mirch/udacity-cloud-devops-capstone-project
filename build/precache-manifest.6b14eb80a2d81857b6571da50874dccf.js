a82ee48a78a4b0d994bfb0044fe77121c626299d
root <root@ip-172-31-35-82.eu-central-1.compute.internal>
2019-09-04 18:40:53 +0000

    Initialize React app

---
+++ build/precache-manifest.6b14eb80a2d81857b6571da50874dccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2425c3338bbfe83650d5cb9143f76e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eaad1b51b02b7386cdc",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f1014e671db4d1e990ea",
+    "url": "/static/js/2.6a672e5a.chunk.js"
+  },
+  {
+    "revision": "3eaad1b51b02b7386cdc",
+    "url": "/static/js/main.9e49b69e.chunk.js"
+  },
+  {
+    "revision": "711e5127095b2715d769",
+    "url": "/static/js/runtime~main.06306b5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
